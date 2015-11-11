3b05d094308cd7a4a511b01e0f2f9dfd1fe5ffa2
Brian Mathews <brian@wintr.us>
2015-11-11 12:59:43 -0800

    Add tests that validate broken Utils.getModifiersForDay call

---
+++ test/WeekdayPicker.js
@@ -0,0 +1,36 @@
+import testDom from "testdom";
+import chai, { expect } from "chai";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+
+chai.use(sinonChai);
+
+testDom("<html><body></body></html>");
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const TestUtils = require("react-addons-test-utils");
+const WeekdayPicker = require("../src/WeekdayPicker");
+
+describe("WeekdayPicker", () => {
+
+  it("renders", () => {
+    const shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(<WeekdayPicker />);
+
+    const weekdayPicker = shallowRenderer.getRenderOutput();
+    expect(weekdayPicker.props.className).to.contain("WeekdayPicker");
+  });
+
+  it ('runs modifiers', () => {
+    const modifiers = {
+      'weekend': function (weekday) {
+        return weekday == 0 || weekday == 6;
+      }
+    };
+
+    const shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(<WeekdayPicker modifiers={modifiers}/>);
+  });
+
+});
